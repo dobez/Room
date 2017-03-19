e135ffa188f66fa9d1c15a1636bbab6656a886a0
Mana Iwakami <dobezbiriketsu@gmail.com>
2017-03-19 17:54:48 +0900

    部屋
    部屋に色が付きました。

--- room.js
+++ room.js
@@ -1,15 +1,19 @@
 'use strict';
-  const makeRoom = ()=>{//部屋を作るやつです
+  const makeRoom = ()=>{//部屋を作るやつ
     const random = (max, min)=>(Math.floor(Math.random() * max) + min);
     const W      = random(39, 22);//はば
     const H      = random(24, 19);//たかさ
-      console.log('soukoban mapwidth:',W,'mapheight:',H);
+      console.log('roomroom mapwidth:',W,'mapheight:',H);
+
     var humanX = random((W - 2), 1);
     var humanY = random((H - 2), 1);
-        console.log('soukoban humanY: ',humanY);
+        console.log('roomroom humanY: ',humanY);
+
     let y;
     let x;
     var room = new Array();//へや
+
+
     //部屋を作る・・・
     for (y = 0; y < H; y++) {
         room[y] = new Array();
@@ -27,9 +31,14 @@
             room[y][x] = 0; }//ゆかは0
           　}
       }
+
+
     let roomStr = room.join();
     return roomStr;
   };
+
+
+
   const roomRendering = function(arg){//
     var OKitisaRoom = arg.replace(/0,/g,"□")
                          .replace(/1,/g,"■")
@@ -37,46 +46,73 @@
                          .replace(/9,/,"▼");
     return OKitisaRoom;
   }
+
   const roomPainter = function(arg){
-    var paintedRoom = arg.replace(/□/g,"<font color=\"\">□<\/font>")
-                         .replace(/■/g,"<font color=\"\">■<\/font>")
-                         .replace(/■<br>/g,"<font color=\"\">■<\/font><br>")
-                         .replace(/▼/g,"<font color=\"\">■<\/font><br>");
+    var paintedRoom = arg.replace(/□/g,"<font color=\"#cca\">□<\/font>")
+                         .replace(/■/g,"<font color=\"#3c7\">■<\/font>")
+                         .replace(/■<br>/g,"<font color=\"#a8a\">■<\/font><br>")
+                         .replace(/▼/g,"<font color=\"#a3a\">▼<\/font>");
     return paintedRoom;
   }
+
   const roomPaintRemover = function(arg){
-    var plainRoom = arg.replace(/<font color=\"\">□<\/font>/g,"□")
-                       .replace(/<font color=\"\">■<\/font>/g,"■")
-                       .replace(/<font color=\"\">■<\/font><br>/g,"■<br>")
-                       .replace(/<font color=\"\">■<\/font><br>/g,"▼");
+    var plainRoom = arg.replace(/<font color=\"#cca\">□<\/font>/g,"□")
+                       .replace(/<font color=\"#3c7\">■<\/font>/g,"■")
+                       .replace(/<font color=\"#a8a\">■<\/font><br>/g,"■<br>")
+                       .replace(/<font color=\"#a3a\">▼<\/font>/g,"▼");
     return plainRoom;
   }
+
+
   document.addEventListener("DOMContentLoaded",function(event){
-    document.yosoukoban = document.getElementById('yo');
-    document.yosoukoban.insertAdjacentHTML('beforeend',roomRendering(makeRoom()));
+      document.yoroomroom = document.getElementById('yo');
+      document.yoroomroom.insertAdjacentHTML('beforeend',roomPainter(roomRendering(makeRoom())));
   },false)
+
+
   window.addEventListener('keydown',roomKeydown);
+
   function roomKeydown(event){
-      event.preventDefault();
-      document.yosoukoban = document.getElementById('yo');
-      if(event.keyCode == 38)　{
-        document.yosoukoban.innerHTML =
-        document.yosoukoban.innerHTML.replace(/(■□+■<br>)(■□*▼□*■<br>)/,"$2$1");
-        //console.log("↑");
-      }
+        event.preventDefault();
+
+        var workSpace = "";
+        var material = document.yoroomroom.innerHTML;
+        document.yoroomroom = document.getElementById('yo');
+
+        if(event.keyCode == 38)　{
+            workSpace = roomPainter(
+                        roomPaintRemover(material.toString())
+                        .replace(/(■□+■<br>)(■□*▼□*■<br>)/,"$2$1")
+                        );
+            document.yoroomroom.innerHTML =
+            workSpace;
+            //console.log("↑");
+        }
       if(event.keyCode == 40)  {
-        document.yosoukoban.innerHTML =
-        document.yosoukoban.innerHTML.replace(/(■□*▼□*■<br>)(■□+■<br>)/,"$2$1");
+        workSpace = roomPainter(
+                    roomPaintRemover(material.toString())
+                    .replace(/(■□*▼□*■<br>)(■□+■<br>)/,"$2$1")
+                    );
+        document.yoroomroom.innerHTML =
+        workSpace;
         //console.log("↓");
       }
       if(event.keyCode == 39)  {
-        document.yosoukoban.innerHTML =
-        document.yosoukoban.innerHTML.replace(/▼□/,"□▼");
+        workSpace = roomPainter(
+                    roomPaintRemover(material.toString())
+                    .replace(/▼□/,"□▼")
+                    );
+        document.yoroomroom.innerHTML =
+        workSpace;
         //console.log("→");
       }
       if(event.keyCode == 37)　{
-        document.yosoukoban.innerHTML =
-        document.yosoukoban.innerHTML.replace(/□▼/,"▼□");
+        workSpace = roomPainter(
+                    roomPaintRemover(material.toString())
+                    .replace(/□▼/,"▼□")
+                    );
+        document.yoroomroom.innerHTML =
+        workSpace;
         //console.log("←");
       }
       }
